42c606a251a8a3cbe42400995d4a231420f53618
So1968 <sofia.rios@gmx.fr>
2026-05-27 20:28:43 +0200

    Charge le theme retro annees 50 dans l application

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './ux-feedback.css'
+import './retro-50.css'
 import './referents-extension.js'
 import App from './App.jsx'
 
